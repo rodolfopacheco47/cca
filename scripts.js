6761c40cfba585448a3b5410f5a4309a32da5dbf
rodolfopacheco47 <45245984+rodolfopacheco47@users.noreply.github.com>
2019-07-30 10:45:12 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var options = {
+    collectionWrapperSelector: 'ul',
+    leftBtnSelector: '.lc-arrow-left',
+    rightBtnSelector: '.lc-arrow-right',
+    scrollbarTrackSelector: '.scrollbar-track'
+  }
+
+var wrapper = $('#carousel');
+
+var lc = new LightCarousel(wrapper, options);
